ae1cd68991f2e8e5ef57108ce77484b1559b58a0
Creator <creator@localhost.localdomain>
2013-10-29 02:00:10 -0400

    Moving all the code in

---
+++ rover.js
@@ -0,0 +1,43 @@
+var ws = require('ws');
+var gpio = require('pi-gpio');
+
+// Open connection to server - how to not hard-code this?
+controlSocket = new ws('ws://192.168.0.101/controlDrone/');
+
+controlSocket.on('open', function() {
+  console.log('Opened control socket connection');
+});
+
+// All pins used so far are boolean outputs
+function openPin(pinNumber) {
+  gpio.open(pinNumber, 'output', function(error) {
+    if(error) {
+      console.log("Error while trying to open pin " + pinNumber + ": " + error);
+      console.log("Trying again in 10s...");
+      setTimeout(openPin, 10000, pinNumber);
+    }
+    else {console.log("Pin " + pinNumber + " opened with no errors")}
+  });
+}
+openPin( 7);
+openPin(11);
+openPin(12);
+openPin(13);
+openPin(15);
+
+// Listen for state changes for the rover
+controlSocket.on('message', function(message) {
+  console.log('Received from control socket: ' + message);
+  
+  var rover = JSON.parse(message);
+  
+  if(rover.lamp !== undefined) gpio.write(12, Boolean(rover.lamp));
+  if(rover.motorRight !== undefined) {
+    gpio.write( 7, rover.motorRight < 0);
+    gpio.write(11, rover.motorRight > 0);
+  }
+  if(rover.motorLeft !== undefined) {
+    gpio.write(13, rover.motorLeft < 0);
+    gpio.write(15, rover.motorLeft > 0);
+  }
+});
---
+++ server.js
@@ -0,0 +1,142 @@
+var express = require("express");
+var ws = require("ws");
+var readline = require("readline");
+var http = require('http');
+
+//////////////////////////////
+// HTTP server for web page //
+//////////////////////////////
+
+var FILE_PORT = 8080;
+var app = express();
+var httpServer = http.createServer(app);
+
+app.use(express.compress());
+app.use(express.static("./http"));
+httpServer.listen(FILE_PORT);
+
+console.log((new Date()) + ": Static file server listening at http://[all hosts]:" + FILE_PORT + "/");
+
+///////////////////////////
+// Repeater for controls //
+///////////////////////////
+
+/*var controlSocketToThePI = new ws('ws://192.168.0.11:8080/websocket');
+controlSocketToThePI.on('open', function() {
+  console.log('Opened websocket to PI');
+});*/
+
+var controlSocketToThePI = new (ws.Server)({server: httpServer, path: '/controlDrone/'});
+var connectionToPi;
+
+controlSocketToThePI.on('connection', function(connection) {
+  connectionToPi = connection;
+  console.log('Received connection from Pi');
+});
+
+var controlSocketServer = new (ws.Server)({server: httpServer, path: '/controlClient/'});
+
+controlSocketServer.on('connection', function(connection) {
+  connection.on('message', function(message) {
+    console.log('received this: ' + message);
+    
+    try {
+      connectionToPi.send(message);
+    }
+    catch(error) {
+      console.log(error);
+    }
+  });
+});
+
+////////////////////////////////////
+// Video part from those iOS devs //
+////////////////////////////////////
+
+var STREAM_PORT = 8082,
+    STREAM_SECRET = 's3cret', // CHANGE THIS!
+    WEBSOCKET_PORT = 8080,
+    STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
+
+var clients = {};
+var width = 320,
+    height = 240;
+
+// Websocket Server
+var socketServer = new (ws.Server)({server: httpServer, path: '/videoClient/'});
+var _uniqueClientId = 1;
+
+var socketError = function() { /* ignore */ };
+socketServer.on('connection', function(socket) {
+	// Send magic bytes and video size to the newly connected socket
+	// struct {char magic[4]; unsigned short width, height;}
+	var streamHeader = new Buffer(8);
+	streamHeader.write(STREAM_MAGIC_BYTES);
+	streamHeader.writeUInt16BE(width, 4);
+	streamHeader.writeUInt16BE(height, 6);
+	socket.send(streamHeader, {binary:true}, socketError);
+
+	// Remember client in 'clients' object
+	var clientId = _uniqueClientId++;
+	clients[clientId] = socket;
+	console.log(
+		'WebSocket Connect: client #' + clientId + 
+		' ('+Object.keys(clients).length+' total)'
+	);
+
+	// Delete on close
+	socket.on('close', function(code, message){
+		delete clients[clientId];
+		console.log(
+			'WebSocket Disconnect: client #' + clientId +
+			' ('+Object.keys(clients).length+' total)'
+		);
+	});
+});
+
+
+// HTTP Server to accept incomming MPEG Stream
+var streamServer = require('http').createServer( function(request, response) {
+	var params = request.url.substr(1).split('/');
+	width = (params[1] || 320)|0;
+	height = (params[2] || 240)|0;
+
+	if( params[0] == STREAM_SECRET ) {
+		console.log(
+			'Stream Connected: ' + request.socket.remoteAddress + 
+			':' + request.socket.remotePort + ' size: ' + width + 'x' + height
+		);
+		request.on('data', function(data){
+			for( c in clients ) {
+				clients[c].send(data, {binary:true}, socketError);
+			}
+		});
+	}
+	else {
+		console.log(
+			'Failed Stream Connection: '+ request.socket.remoteAddress + 
+			request.socket.remotePort + ' - wrong secret.'
+		);
+		response.end();
+	}
+}).listen(STREAM_PORT);
+
+console.log('Listening for MPEG Stream on http://127.0.0.1:'+STREAM_PORT+'/<secret>/<width>/<height>');
+console.log('Awaiting WebSocket connections on ws://127.0.0.1:'+WEBSOCKET_PORT+'/');
+
+/////////
+// CLI //
+/////////
+
+try {
+  var CLILink = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  CLILink.on("line", function(message) {
+    try {console.log(eval(message));}
+    catch(error) {console.log(error);}
+  });
+}
+catch(error) {/* When we fail, we just give up, dying in the depths of an interpreter, not even crying out an error */}
