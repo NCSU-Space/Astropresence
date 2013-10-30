e7b8e65b2dbca190a6a0408e0a252851790e9148
Densaugeo <nathan.yinger@gmail.com>
2013-10-30 11:48:16 -0400

    Fixed some white space and comments

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var FILE_PORT = 8080;
 var app = express();
 var httpServer = http.createServer(app);
 
+// Simple static page server
 app.use(express.compress());
 app.use(express.static("./http"));
 httpServer.listen(FILE_PORT);
@@ -21,11 +22,7 @@ console.log((new Date()) + ": Static file server listening at http://[all hosts]
 // Repeater for controls //
 ///////////////////////////
 
-/*var controlSocketToThePI = new ws('ws://192.168.0.11:8080/websocket');
-controlSocketToThePI.on('open', function() {
-  console.log('Opened websocket to PI');
-});*/
-
+// These link to rover side
 var controlSocketToThePI = new (ws.Server)({server: httpServer, path: '/controlDrone/'});
 var connectionToPi;
 
@@ -34,6 +31,7 @@ controlSocketToThePI.on('connection', function(connection) {
   console.log('Received connection from Pi');
 });
 
+// And these link to client(s)
 var controlSocketServer = new (ws.Server)({server: httpServer, path: '/controlClient/'});
 
 controlSocketServer.on('connection', function(connection) {
@@ -68,57 +66,45 @@ var _uniqueClientId = 1;
 
 var socketError = function() { /* ignore */ };
 socketServer.on('connection', function(socket) {
-	// Send magic bytes and video size to the newly connected socket
-	// struct {char magic[4]; unsigned short width, height;}
-	var streamHeader = new Buffer(8);
-	streamHeader.write(STREAM_MAGIC_BYTES);
-	streamHeader.writeUInt16BE(width, 4);
-	streamHeader.writeUInt16BE(height, 6);
-	socket.send(streamHeader, {binary:true}, socketError);
-
-	// Remember client in 'clients' object
-	var clientId = _uniqueClientId++;
-	clients[clientId] = socket;
-	console.log(
-		'WebSocket Connect: client #' + clientId + 
-		' ('+Object.keys(clients).length+' total)'
-	);
-
-	// Delete on close
-	socket.on('close', function(code, message){
-		delete clients[clientId];
-		console.log(
-			'WebSocket Disconnect: client #' + clientId +
-			' ('+Object.keys(clients).length+' total)'
-		);
-	});
+  // Send magic bytes and video size to the newly connected socket
+  // struct {char magic[4]; unsigned short width, height;}
+  var streamHeader = new Buffer(8);
+  streamHeader.write(STREAM_MAGIC_BYTES);
+  streamHeader.writeUInt16BE(width, 4);
+  streamHeader.writeUInt16BE(height, 6);
+  socket.send(streamHeader, {binary:true}, socketError);
+  
+  // Remember client in 'clients' object
+  var clientId = _uniqueClientId++;
+  clients[clientId] = socket;
+  console.log('WebSocket Connect: client #' + clientId + ' ('+Object.keys(clients).length+' total)');
+  
+  // Delete on close
+  socket.on('close', function(code, message) {
+    delete clients[clientId];
+    console.log('WebSocket Disconnect: client #' + clientId + ' ('+Object.keys(clients).length+' total)');
+  });
 });
 
 
 // HTTP Server to accept incomming MPEG Stream
 var streamServer = require('http').createServer( function(request, response) {
-	var params = request.url.substr(1).split('/');
-	width = (params[1] || 320)|0;
-	height = (params[2] || 240)|0;
-
-	if( params[0] == STREAM_SECRET ) {
-		console.log(
-			'Stream Connected: ' + request.socket.remoteAddress + 
-			':' + request.socket.remotePort + ' size: ' + width + 'x' + height
-		);
-		request.on('data', function(data){
-			for( c in clients ) {
-				clients[c].send(data, {binary:true}, socketError);
-			}
-		});
-	}
-	else {
-		console.log(
-			'Failed Stream Connection: '+ request.socket.remoteAddress + 
-			request.socket.remotePort + ' - wrong secret.'
-		);
-		response.end();
-	}
+  var params = request.url.substr(1).split('/');
+  width = (params[1] || 320)|0;
+  height = (params[2] || 240)|0;
+  
+  if( params[0] == STREAM_SECRET ) {
+    console.log('Stream Connected: ' + request.socket.remoteAddress + ':' + request.socket.remotePort + ' size: ' + width + 'x' + height);
+    request.on('data', function(data) {
+      for( c in clients ) {
+        clients[c].send(data, {binary:true}, socketError);
+      }
+    });
+  }
+  else {
+    console.log('Failed Stream Connection: '+ request.socket.remoteAddress + request.socket.remotePort + ' - wrong secret.');
+    response.end();
+  }
 }).listen(STREAM_PORT);
 
 console.log('Listening for MPEG Stream on http://127.0.0.1:'+STREAM_PORT+'/<secret>/<width>/<height>');
