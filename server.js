12c80a160ce0a582074e0b5be2036ed0430e56e0
Densaugeo <nathan.yinger@gmail.com>
2014-01-26 19:42:28 -0500

    Refactor and prune

--- server.js
+++ server.js
@@ -1,46 +1,45 @@
-var express = require('express');
-var ws = require('ws');
-var readline = require('readline');
-var http = require('http');
-var repl = require('repl');
-
 process.title = 'tele_server';
 
-//////////////////////////////
-// HTTP server for web page //
-//////////////////////////////
+var http = require('http');
+var repl = require('repl');
+var express = require('express');
+var ws = require('ws');
 
-var FILE_PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+/////////////////
+// HTTP server //
+/////////////////
+
+var PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var IP = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var app = express();
-var httpForStatics = http.createServer(app);
-//var httpForWS = http.createServer();
-//httpForWS.listen(8000, IP);
-
+var httpServer = http.createServer(app);
+httpServer.listen(PORT, IP);
 
 // Simple static page server
 app.use('/http', express.static('./http'));
 app.use(express.compress());
-httpForStatics.listen(FILE_PORT, IP);
-
-console.log((new Date()) + ': Static file server listening at http://' + IP + ':' + FILE_PORT + '/');
+console.log(new Date().toUTCString() + ': Static file server listening at http://' + IP + ':' + PORT + '/http');
 
 ///////////////////////////
 // Repeater for controls //
 ///////////////////////////
 
 // These link to rover side
-var controlSocketToThePI = new (ws.Server)({server: httpForStatics, path: '/controlDrone'});
+var controlSocketToThePI = new ws.Server({server: httpServer, path: '/controlDrone'});
 
 controlSocketToThePI.on('connection', function(connection) {
   console.log('Received connection from Pi');
+  
+  connection.on('close', function() {
+    console.log(new Date().toUTCString() + ': Closed connection from Pi');
+  });
 });
 
 // And these link to client(s)
-var controlSocketServer = new (ws.Server)({server: httpForStatics, path: '/controlClient'});
+var controlSocketServer = new ws.Server({server: httpServer, path: '/controlClient'});
 
 controlSocketServer.on('connection', function(connection) {
-  console.log((new Date()) + ': Received WebSocket at :8000/controlClient');
+  console.log(new Date().toUTCString() + ': Received WebSocket at :' + PORT + '/controlClient');
   
   connection.on('message', function(message) {
     try {
@@ -52,7 +51,7 @@ controlSocketServer.on('connection', function(connection) {
   });
   
   connection.on('close', function() {
-    console.log((new Date()) + ': Closed WebSocket at :8000/controlClient');
+    console.log(new Date().toUTCString() + ': Closed WebSocket at :' + PORT + '/controlClient');
   });
 });
 
@@ -70,7 +69,7 @@ var width = 320,
     height = 240;
 
 // Websocket Server
-var socketServer = new (ws.Server)({server: httpForStatics, path: '/videoClient'});
+var socketServer = new (ws.Server)({server: httpServer, path: '/videoClient'});
 var _uniqueClientId = 1;
 
 var socketError = function() {};
@@ -111,9 +110,8 @@ function receiveMPEG(request, response) {
 }
 
 app.use(STREAM_SECRET, receiveMPEG);
-//).listen(8082, IP);
 
-console.log('Listening for MPEG Stream on http://' + IP + ':' + FILE_PORT + '/<secret>/<width>/<height>');
+console.log('Listening for MPEG Stream on http://' + IP + ':' + PORT + '/<secret>/<width>/<height>');
 
 /////////
 // CLI //
@@ -123,8 +121,6 @@ console.log('Listening for MPEG Stream on http://' + IP + ':' + FILE_PORT + '/<s
 
 var cli = repl.start({});
 cli.context.app = app;
-cli.context.httpForStatics = httpForStatics;
+cli.context.httpServer = httpServer;
 cli.context.controlSocketToThePI = controlSocketToThePI;
 cli.context.controlSocketServer = controlSocketServer;
-//cli.context.clients = clients;
-//cli.context.socketServer = socketServer;
