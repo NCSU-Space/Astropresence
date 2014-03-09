75cf1fdf95f128a4ba574acb836d2254a9bc604e
Densaugeo <nathan.yinger@gmail.com>
2014-03-09 15:57:18 -0400

    Arduino PWM testing

--- rover.js
+++ rover.js
@@ -1,5 +1,5 @@
 var ws = require('ws');
-var gpio = require('pi-gpio');
+var fs = require('fs');
 var repl = require('repl');
 
 process.title = 'tele_client_on_drone';
@@ -16,22 +16,7 @@ controlSocket.on('open', function() {
   console.log('Opened control socket connection');
 });
 
-// All pins used so far are boolean outputs
-function openPin(pinNumber) {
-  gpio.open(pinNumber, 'output', function(error) {
-    if(error) {
-      console.log('Error while trying to open pin ' + pinNumber + ': ' + error);
-      console.log('Trying again in 10s...');
-      setTimeout(openPin, 10000, pinNumber);
-    }
-    else {console.log('Pin ' + pinNumber + ' opened with no errors')}
-  });
-}
-openPin( 7);
-openPin(11);
-openPin(12);
-openPin(13);
-openPin(15);
+var stream = fs.createWriteStream('communication_file');
 
 // Listen for state changes for the rover
 controlSocket.on('message', function(message) {
@@ -39,15 +24,13 @@ controlSocket.on('message', function(message) {
   
   var rover = JSON.parse(message);
   
-  if(rover.lamp !== undefined) gpio.write(12, Boolean(rover.lamp));
-  if(rover.motorRight !== undefined) {
-    gpio.write( 7, rover.motorRight < 0);
-    gpio.write(11, rover.motorRight > 0);
-  }
-  if(rover.motorLeft !== undefined) {
-    gpio.write(13, rover.motorLeft < 0);
-    gpio.write(15, rover.motorLeft > 0);
-  }
+  if(rover.motorRight === 0) stream.write(new Buffer([1, 0, 0]));
+  if(rover.motorRight > 0) stream.write(new Buffer([1, 1, 128]));
+  if(rover.motorRight < 0) stream.write('def');
+  
+  if(rover.motorLeft === 0) stream.write(new Buffer([1, 0, 0]));
+  if(rover.motorLeft > 0) stream.write(new Buffer([1, 1, Math.round(rover.motorLeft)*255]));
+  if(rover.motorLeft < 0) stream.write(new Buffer([1, 2, Math.round(rover.motorLeft)*-255]));
 });
 
 /////////
