953c9096cd4c5f849c6bbc0298725d40cf5f633d
Densaugeo <nathan.yinger@gmail.com>
2014-03-09 21:52:12 -0400

    Options settings for server

--- server.js
+++ server.js
@@ -1,24 +1,72 @@
 process.title = 'tele_server';
 
-var http = require('http');
-var repl = require('repl');
+var http    = require('http');
+var repl    = require('repl');
+var fs      = require('fs');
 var express = require('express');
-var ws = require('ws');
+var ws      = require('ws');
+var stdio   = require('stdio');
+var iz      = require('iz');
+
+/////////////////
+// Get options //
+/////////////////
+
+var optionsCLI = stdio.getopt({
+  'ip'    : {key: 'i', args: 1, description: 'IP address for both HTTP and WS servers. Defaults to OpenShift if available, or if not then 0.0.0.0'},
+  'port'  : {key: 'p', args: 1, description: 'TCP port for both HTTP and WS servers. Defaults to OpenShift if available, or if not then 8080'},
+  'config': {key: 'c', args: 1, description: 'Load settings from configurations file. Defaults to ./config.json'},
+  'nofile': {          args: 0, description: 'Run without a config file. "ip" and "port" options must be specified'}
+});
+
+try {
+  var optionsFile = JSON.parse(fs.readFileSync(optionsCLI.config || 'config.json'));
+}
+catch(error) {
+  console.error('Warning: Unable to read config file "' + (optionsCLI.config || 'config.json') + '". (' + error + '). Options not specified by argument will fall back to hard-coded defaults');
+  var optionsFile = {}
+}
+
+var optionsDefault = {
+  ip    : '0.0.0.0',
+  port  : 8080,
+  config: null,
+  nofile: true
+};
+
+// Condense options sources into one options object, applying priority
+var options = {};
+
+for(var i in optionsDefault) {
+  options[i] = optionsCLI[i] || optionsFile[i] || optionsDefault[i];
+}
+
+if(!iz(options.ip).ip().valid) {
+  console.error('Error: IP must be a valid IP address or local url');
+  process.exit(1);
+}
+
+if(!iz(options.port).int().between(1, 65535).valid) {
+  console.error('Error: TCP port must be an integer between 1 and 65535');
+  process.exit(1);
+}
+
+if(iz(options.port).int().between(1, 1024).valid) {
+  console.warn('Warning: TCP ports between 1 and 1024 may require root permission');
+}
 
 /////////////////
 // HTTP server //
 /////////////////
 
-var PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var IP = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var app = express();
 var httpServer = http.createServer(app);
-httpServer.listen(PORT, IP);
+httpServer.listen(options.port, options.ip);
 
 // Simple static page server
 app.use('/http', express.static('./http'));
 app.use(express.compress());
-console.log(new Date().toUTCString() + ': Static file server listening at http://' + IP + ':' + PORT + '/http');
+console.log(new Date().toUTCString() + ': Static file server listening at http://' + options.ip + ':' + options.port + '/http');
 
 ///////////////////////////
 // Repeater for controls //
@@ -39,7 +87,7 @@ controlSocketToThePI.on('connection', function(connection) {
 var controlSocketServer = new ws.Server({server: httpServer, path: '/controlClient'});
 
 controlSocketServer.on('connection', function(connection) {
-  console.log(new Date().toUTCString() + ': Received WebSocket at :' + PORT + '/controlClient');
+  console.log(new Date().toUTCString() + ': Received WebSocket at :' + options.port + '/controlClient');
   
   connection.on('message', function(message) {
     try {
@@ -51,7 +99,7 @@ controlSocketServer.on('connection', function(connection) {
   });
   
   connection.on('close', function() {
-    console.log(new Date().toUTCString() + ': Closed WebSocket at :' + PORT + '/controlClient');
+    console.log(new Date().toUTCString() + ': Closed WebSocket at :' + options.port + '/controlClient');
   });
 });
 
@@ -111,7 +159,7 @@ function receiveMPEG(request, response) {
 
 app.use(STREAM_SECRET, receiveMPEG);
 
-console.log('Listening for MPEG Stream on http://' + IP + ':' + PORT + '/<secret>/<width>/<height>');
+console.log('Listening for MPEG Stream on http://' + options.ip + ':' + options.port + '/<secret>/<width>/<height>');
 
 /////////
 // CLI //
