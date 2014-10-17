c80b7c808c5091556f8cba4e5ec963909865e661
Densaugeo <you@example.com>
2014-10-17 16:57:03 -0400

    New microcontroller connections

--- rover.js
+++ rover.js
@@ -1,36 +1,53 @@
-var ws = require('ws');
-var fs = require('fs');
-var repl = require('repl');
+var ws    = require('ws');
+var repl  = require('repl');
+var fs    = require('fs');
+var stdio = require('stdio');
 
 process.title = 'tele_client_on_drone';
 
-if(process.argv[2] === undefined) {
-  console.log('Usage: node rover.js [Server IP]:[WebSocket port]');
-  console.log('If you don\'t know which port the server is using, it\'s probably 8080');
+var optionsAllowed = {
+  'ip'  : {key: 'i', args: 1, mandatory: true, description: 'IP adress or hostname of server'},
+  'port': {key: 'p', args: 1, mandatory: true, description: 'TCP port number for control websocket (usually 8080)'},
+  'pipe': {key: 'e', args: 1, mandatory: true, description: 'Name of pipe for communicating with microcontroller'},
+};
+
+var options = stdio.getopt(optionsAllowed);
+
+// Doesn't seem to actually catch errors...maybe there is an option to place an event handler with the contructor?
+try {
+  controlSocket = new ws('ws://' + options.ip + ':' + options.port + '/controlDrone');
+}
+catch(e) {
+  console.log('Error opening control socket: ' + e);
+  process.exit(1);
 }
 
-// Open connection to server - how to not hard-code this?
-controlSocket = new ws('ws://' + process.argv[2] + '/controlDrone');
+try {
+  var stream = fs.createWriteStream(options.pipe);
+}
+catch(e) {
+  console.log('Error opening pipe for microcontroller: ' + e);
+  process.exit(1);
+}
 
 controlSocket.on('open', function() {
   console.log('Opened control socket connection');
 });
 
-var stream = fs.createWriteStream('communication_file');
-
 // Listen for state changes for the rover
 controlSocket.on('message', function(message) {
   console.log('Received from control socket: ' + message);
   
-  var rover = JSON.parse(message);
+  var mObject = JSON.parse(message);
   
-  if(rover.motorRight === 0) stream.write(new Buffer([1, 0, 0]));
-  if(rover.motorRight > 0) stream.write(new Buffer([1, 1, 128]));
-  if(rover.motorRight < 0) stream.write('def');
-  
-  if(rover.motorLeft === 0) stream.write(new Buffer([1, 0, 0]));
-  if(rover.motorLeft > 0) stream.write(new Buffer([1, 1, Math.round(rover.motorLeft)*255]));
-  if(rover.motorLeft < 0) stream.write(new Buffer([1, 2, Math.round(rover.motorLeft)*-255]));
+  switch(mObject.command) {
+    case 'motors':
+      var motors = mObject.motors;
+      for(var i = 1, endi = 9; i < endi; ++i) {
+        stream.write(new Buffer([i, motors[i] > 0 ? 1 : 2, Math.floor(Math.abs(motors[i]), 255)]));
+      }
+      break;
+  }
 });
 
 /////////
@@ -39,3 +56,4 @@ controlSocket.on('message', function(message) {
 
 var cli = repl.start({});
 cli.context.controlSocket = controlSocket;
+cli.context.stream = stream;
